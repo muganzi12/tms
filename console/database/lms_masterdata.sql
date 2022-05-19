06cc45514ccae5b01d6f7c3b4f91a2252a246f61
Robin Muganzi <36589002+muganzi12@users.noreply.github.com>
2022-05-19 09:27:26 +0300

    Database

--- console/database/lms_masterdata.sql
+++ console/database/lms_masterdata.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Aug 16, 2021 at 11:55 AM
+-- Generation Time: May 19, 2022 at 06:24 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -112,7 +112,9 @@ INSERT INTO `master_data` (`id`, `name`, `description`, `created_by`, `created_a
 (33, 'Income', 'Income', '2', NULL, NULL, NULL, 'account_type', NULL, NULL),
 (34, 'Liability', 'Liability', '2', NULL, NULL, NULL, 'account_type', NULL, NULL),
 (35, 'UGX', 'UGX', '2', NULL, NULL, NULL, 'currency', NULL, NULL),
-(36, 'Rejected', 'Rejected', '2', NULL, NULL, NULL, NULL, NULL, 'danger');
+(36, 'Rejected', 'Rejected', '2', NULL, NULL, NULL, NULL, NULL, 'danger'),
+(37, 'Divorced', 'Divorced', '2', 1634799270, NULL, '', 'marital_status', NULL, ''),
+(46, 'CS', 'CS', '2', 1634801190, NULL, '', 'client_type', NULL, '');
 
 -- --------------------------------------------------------
 
@@ -154,15 +156,23 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `username`, `firstname`, `lastname`, `othername`, `auth_key`, `password_hash`, `password_status`, `password_reset_token`, `email`, `status`, `client_id`, `branch_id`, `created_at`, `updated_at`, `verification_token`, `profile_pic`, `office_id`, `is_admin`, `app_module`, `telephone`, `login_at`, `passwrd_reset_at`, `created_by`, `updated_by`) VALUES
 (1, 'rmuganzi1', 'Robin', 'Muganzi', '', 'rYf5_CeqEB6WOX3buPugK789jv39mSZo', '$2y$10$BA/3Dlz5UGA9dWRBxkUwu..j1eGYVIMoJ7Sl20rpE8CxiU2QTztna', 0, 'R2OJOgNasmkguVkIAI2NMeOa5JLvrxPP_1621077365', 'muganzirobin58@gmail.com', 1, 1, 0, 1587360980, 1628589621, '62AKLupZAZ1OTdBcd-xT-956WbKgdswu_1587360980', NULL, 0, 1, 1, '0789588332', NULL, NULL, 2, 1),
-(2, 'pashaba', 'Penlope', 'Ashaba', '', 'OZnerVEqyfoA0NwoLvyVhXhAgl0GKd21', '$2y$10$BA/3Dlz5UGA9dWRBxkUwu..j1eGYVIMoJ7Sl20rpE8CxiU2QTztna', 0, '31IjUfphd322LxLRTitUMddsOuGDamxa_1628260313', 'penlopeashaba5@gmail.com', 1, 1, 1, 1628260313, 1628359217, NULL, NULL, NULL, 1, 2, '0789588332', NULL, NULL, 1, 2),
-(3, 'jalex', 'Alex', 'Junior', '', '3kkpWU7fexDJLrjnu7rKUPHbA90zvnR-', '$2y$13$XiGnrM0SYQKILNAn4KtC7OrL.6Sa/BpZXnUI0mDWHEYgnmgZF66VG', 0, 'lYLBpyuW0JVls-97BYjItPWzdr7L_Lmy_1628261359', 'muganzirobin7@gmail.com', 1, 1, 1, 1628261359, 1628359211, NULL, NULL, NULL, 1, 2, '07895883456', NULL, NULL, 1, 2),
-(4, 'amugume', 'Alex', 'Mugume', '', '0FOGLJBnFDJwNSlWlM9I09_w45cpBASh', '$2y$13$G/bDyEPv4mNb1c084UlI1ONMvVGFb2/m4/OAo0Wo1a2Q3qBKzfGge', 0, 'ihPy5lTpuDPKUbF5PHhleTa56t4aiMh2_1628261638', 'muganzirobin8@gmail.com', 1, 1, 1, 1628261638, 1628590175, NULL, NULL, NULL, 1, 2, '0750588332', NULL, NULL, 1, 8),
+(4, 'amugume', 'Alex', 'Mugume', '', '0FOGLJBnFDJwNSlWlM9I09_w45cpBASh', '$2y$13$G/bDyEPv4mNb1c084UlI1ONMvVGFb2/m4/OAo0Wo1a2Q3qBKzfGge', 0, 'ihPy5lTpuDPKUbF5PHhleTa56t4aiMh2_1628261638', 'muganzirobin8@gmail.com', 1, 1, 1, 1628261638, 1634133678, NULL, NULL, NULL, 1, 2, '0750588332', NULL, NULL, 1, 2),
 (5, 'bmutebi', 'Bruce', 'Mutebi', 'Kapo', 'ZyGfZFqSdXqivYtJ7bLz5w6R5Do8dhMD', '$2y$13$QuG7gPY0Z.Q5BPAEYwQoP.ucE99cgF3lgvyiriOmNpcx6TyuflUCK', 0, 'X7GbAWQDBNAKop4dCevkNkvx2sHPMwya_1628262782', 'muganzirobin60@gmail.com', 1, 1, 4, 1628262782, 1628360544, NULL, NULL, NULL, 1, 2, '0789588332', NULL, NULL, 1, 2),
-(6, 'pnakaali', 'Phionah', 'Nakaali', '', '-YUfAiv3hkg-IkF9u9CAEgNldMqVZRNf', '$2y$13$mPaewVC.3Cm6aVOUrQAeq.zNz87Z7cuijwfxLJMkNiDjizXADyogK', 0, 'elFoQlj5qUrUp1wv2xs55PHc6FyoLsp7_1628275953', 'muganzirobin68@gmail.com', 1, 1, 1, 1628275953, 1628590180, NULL, NULL, NULL, 1, 2, '0789588332', NULL, NULL, 1, 8),
-(7, 'pnakaali15', 'Phionah', 'Nakaali', 'Nazziwa', 'arEuzMcm7_jCQyQrxkmxOiTvu0hLMYLU', '$2y$13$0T0GUJ.cneXvGfw5XbzbbO3Ft.dEDNSvb1CXF6BEUJIzUSygUC.3O', 0, 'QoGAzAuhUnAsHZe_if9EY3lzz9oH0j8y_1628577997', 'phionahnakaali4@gmail.com', 2, 2, 1, 1628577997, 1628589663, NULL, NULL, NULL, 1, 2, '0789054637', NULL, NULL, 1, 2),
-(8, 'njuluis', 'Julius ', 'Nabibanya', '', 'YCRkFyuyMlNWBcYXDb7jYYggd5cKufQr', '$2y$13$s79IlshMnZiTQDiXUpnIReKHcGHoEgUs3BQjAvaHUIzIDTTedXy1.', 0, 'xw8UsfRR-M2_umoLkG4yxK4rXlQKuHIM_1628589933', 'muganzirobin62@gmail.com', 1, 1, 1, 1628589933, 1628589933, NULL, NULL, NULL, 0, 2, '0765005643', NULL, NULL, 2, NULL),
-(9, 'kpaul', 'Paul', 'Kagame', '', '3kucAXFyDHr-kTIp1CHrYsaJKo7nEj3p', '$2y$13$mG2/vsV.VhYTInkZpoGsbOjGWUflv8B4J1Xw0eq.RzCbZQUH1PSZW', 0, 'PdZjsETc_wG5RNgk1nR5v-0-7seeB-XS_1628671568', 'muganzirobin50@gmail.com', 1, 0, NULL, 1628671568, 1628671568, NULL, NULL, NULL, 1, 1, '0789546345', NULL, NULL, 1, NULL),
-(10, 'mlex123', 'Mugume', 'Alex', '', 'mEW6RGRlPqrt6lwKbFVRrAPdSfcwC2ie', '$2y$13$vuuYds5cvy2qSKGl9onSOuS0ksjwvhJ.y75XaVzkVCWvEC10X1DcO', 0, 'pnihTubA5Yhc7uyyJEvrso12uaBUgRQa_1628671788', 'muganzirobin69@gmail.com', 1, 3, NULL, 1628671788, 1628671788, NULL, NULL, NULL, 1, 2, '0789435467', NULL, NULL, 9, NULL);
+(6, 'pnakaali', 'Phionah', 'Nakaali', '', '-YUfAiv3hkg-IkF9u9CAEgNldMqVZRNf', '$2y$13$mPaewVC.3Cm6aVOUrQAeq.zNz87Z7cuijwfxLJMkNiDjizXADyogK', 0, 'elFoQlj5qUrUp1wv2xs55PHc6FyoLsp7_1628275953', 'muganzirobin68@gmail.com', 1, 1, 1, 1628275953, 1629969113, NULL, NULL, NULL, 1, 2, '0789588332', NULL, NULL, 1, 1),
+(7, 'pnakaali15', 'Phionah', 'Nakaali', 'Nazziwa', 'arEuzMcm7_jCQyQrxkmxOiTvu0hLMYLU', '$2y$13$0T0GUJ.cneXvGfw5XbzbbO3Ft.dEDNSvb1CXF6BEUJIzUSygUC.3O', 0, 'QoGAzAuhUnAsHZe_if9EY3lzz9oH0j8y_1628577997', 'phionahnakaali4@gmail.com', 2, 2, 1, 1628577997, 1629969042, NULL, NULL, NULL, 1, 2, '0789054637', NULL, NULL, 1, 1),
+(8, 'njuluis', 'Julius ', 'Nabibanya', '', 'YCRkFyuyMlNWBcYXDb7jYYggd5cKufQr', '$2y$13$s79IlshMnZiTQDiXUpnIReKHcGHoEgUs3BQjAvaHUIzIDTTedXy1.', 0, 'xw8UsfRR-M2_umoLkG4yxK4rXlQKuHIM_1628589933', 'muganzirobin62@gmail.com', 1, 1, 1, 1628589933, 1634133485, NULL, NULL, NULL, 0, 2, '0765005643', NULL, NULL, 2, 2),
+(9, 'kpaul', 'Paul', 'Kagame', '', '3kucAXFyDHr-kTIp1CHrYsaJKo7nEj3p', '$2y$13$mG2/vsV.VhYTInkZpoGsbOjGWUflv8B4J1Xw0eq.RzCbZQUH1PSZW', 0, 'PdZjsETc_wG5RNgk1nR5v-0-7seeB-XS_1628671568', 'muganzirobin50@gmail.com', 1, 1, NULL, 1628671568, 1628671568, NULL, NULL, NULL, 1, 1, '0789546345', NULL, NULL, 1, NULL),
+(10, 'mlex123', 'Mugume', 'Alex', '', 'mEW6RGRlPqrt6lwKbFVRrAPdSfcwC2ie', '$2y$13$vuuYds5cvy2qSKGl9onSOuS0ksjwvhJ.y75XaVzkVCWvEC10X1DcO', 0, 'pnihTubA5Yhc7uyyJEvrso12uaBUgRQa_1628671788', 'muganzirobin69@gmail.com', 1, 3, NULL, 1628671788, 1628671788, NULL, NULL, NULL, 1, 2, '0789435467', NULL, NULL, 9, NULL),
+(11, 'nmugisha', 'Nobert', 'Mugisha', '', 'pWjLKoP4FpDJ_ZNE35slssMFkC-jvpXU', '$2y$10$VkO8wynz1UWqsSj8zyE0p.tQf6MPMAkOMmBrXfLPP.iDrblGFIZAG', 0, 'kfdB-biIhdRH2F_yNvaPiWdKfX9itXUD_1633639987', 'muganzirobin544@gmail.com', 1, 1, 1, 1633639987, 1634134675, NULL, NULL, NULL, 0, 2, '07894675768', NULL, NULL, 2, 2),
+(12, 'n8mugisha', 'Nobert', 'Mugisha', '', 'rDSWsqFM7xNTtOJ8bY6TxkRgjCN2kDav', '$2y$13$KqJbmrI5IsX5F02oNHM6EeXPTkpA5DuIq/yYko3jQFD315IbCm9Ey', 0, 'qRjp0hIim57Wr5vSmTKqO3nzRIn-GWjq_1633640523', 'muganzirobin690@gmail.com', 1, 1, 1, 1633640523, 1645951799, NULL, NULL, NULL, 0, 2, '07894675768', NULL, NULL, 2, 13),
+(13, 'rmugisha', 'Nobert', 'Mugisha', '', 'iaL2dsbRFO9850qu9MbKRxtk8xjzLy6L', '$2y$10$a6.k2HBckpcTD.SsyeLbH.Fl43HRqZsuMKDb3lQFGwiKLdAf4eg7G', 0, NULL, 'muganzirobin5@gmail.com', 1, 1, 1, 1633640834, 1648204355, NULL, 'rmugisha_pic.jpeg', 2, 0, 2, '07894675768', NULL, NULL, 2, 13),
+(15, 'cmathew', 'Mathew', 'Muganzi', '', '2jcARINBvDhCiuRGQjCNhyL4mv6Yaz90', '$2y$10$CgeU5wfnBfCYnAJtlpqiw.l.RV90/elx125oT48YQ9XxjSjdjhzvu', 0, 'efPSPQ2WyhYWz6rAjVmWHnvJvohw9dXE_1643780761', 'muganzirobin545@gmail.com', 1, 2, 1, 1643780761, 1652681575, NULL, NULL, 1, 1, 2, '0789543324', NULL, NULL, 13, 15),
+(16, 'pmuganzi', 'Robin', 'Muganzi', '', 'vjjPt1q8O3xWVW_xHB5ktjMGkbGdhXEq', '$2y$13$6llmhouhQ1rZb13FjxTJGevxw6jKOtY3cjShr2Jb6Y17loZagHHIq', 0, '-CIAffSCOSfWUqrGOs-IXLL2J_1eYUjM_1645949754', 'muganzirobin67@gmail.com', 1, 1, 1, 1645949754, 1645951721, NULL, NULL, NULL, 0, 2, '0745334467', NULL, NULL, 13, 13),
+(17, 'mmuganzi', 'Muganzi', 'Mathew', 'Paul', 'ynARtNFabHMCF4hLILHt-n_22u1TiNyl', '$2y$13$amkHpN4gjAenGTdpmLceT.l9gOOZHfMLnGQJGUJ0JAurU/KqO7XAq', 0, '2EgpL-kMQcKuiCVM_BjX43qANTWMErjQ_1645949988', 'muganzirobin89@gmail.com', 1, 1, 1, 1645949988, 1645951517, NULL, NULL, NULL, 0, 2, '0789467735', NULL, NULL, 13, 13),
+(18, 'probin', 'Robin', 'Paul', '', '61o_9C5XdPoz-x_WxfQjA6AdnbJNC6Kz', '$2y$13$44nnRdzvtuxzqz9WmxCVfe2U5f3ZwU0eG8nfQJnzMo5ycLG1W4crO', 0, 'ssbz6eGDDgDfsHsT7chVDUJVEOP9IzmL_1645950623', 'muganzirobin63@gmail.com', 1, 1, 1, 1645950623, 1645951433, NULL, NULL, NULL, 0, 2, '0789563445', NULL, NULL, 13, 13),
+(19, 'tmagezi', 'Timothy', 'Magagezi', 'Paul', 'Q-oez9IhJpXNBZLvRujVZSTztSQ1sODn', '$2y$13$IUlUf0x6SoKZSyxCX8OW3O2DwT04GVuw62LnGVfbw0LdLwG1hn1Mu', 0, 'QNs7EjIVkUUw7A7emZ7d6MRwd59ruz0z_1646643444', 'muganzirobini7@gmail.com', 1, 1, 1, 1646643444, 1647783360, NULL, NULL, 2, 0, 2, '0789588332', NULL, NULL, 13, 21),
+(20, 'tagaba', 'Timothy', 'Agaba', '', '3WmUmEK6QMkM783_wtlYjI_zLrGaY57W', '$2y$13$6mEZk.rBAlJJuhAuLhbG6.vH7PoU.rfBJDBVzt75cKjQ6PA724FFq', 0, 'RlofXCCJG7cixSSxdNkziWli1Wx0v46i_1647337772', 'muganzirobin890@gmail.com', 1, 1, NULL, 1647337772, 1647337772, NULL, NULL, 1, 0, 2, '0789056734', NULL, NULL, 13, NULL),
+(21, 'kkiggundu', 'Kevin', 'Kiggundu', '', '7ScEh6D4RYfy1bMQIFvSXUb6WBOtBcQR', '$2y$13$pRlwPTIxaJEouYZ536UE9emIMrW4rBGQSRS0zbSrV/yGhYcJT5gOW', 0, 'CvFB6m1hfkivEdJLvZsAgdCvLWl-ihga_1647759198', 'muganzirobin6@gmail.com', 1, 1, NULL, 1647759198, 1647800412, NULL, NULL, 1, 0, 2, '0789054356', NULL, NULL, 13, 21);
 
 --
 -- Indexes for dumped tables
@@ -208,13 +218,13 @@ ALTER TABLE `company`
 -- AUTO_INCREMENT for table `master_data`
 --
 ALTER TABLE `master_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
